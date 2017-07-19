799e1ca14a3f703d3ed251838976e7465353ce4b
akbars95 <Dmitri.Minzat@endava.com>
2017-07-19 17:46:35 +0300

    fixed bug with android orientation

--- android/Application170717/app/src/main/java/android/mtsmda/com/application170717/MainActivity.java
+++ android/Application170717/app/src/main/java/android/mtsmda/com/application170717/MainActivity.java
@@ -6,6 +6,7 @@ import android.mtsmda.com.application170717.helper.ListHelper;
 import android.mtsmda.com.application170717.model.Question;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -13,8 +14,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends MyAppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -30,9 +35,21 @@ public class MainActivity extends MyAppCompatActivity {
     private int mCurrentIndex = 0;
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate called");
         setContentView(R.layout.activity_main);
+
+        if(null != savedInstanceState){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         this.mTrueButton = findViewById(R.id.true_button, Button.class);
         this.mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -75,6 +92,36 @@ public class MainActivity extends MyAppCompatActivity {
                 updateQuestion();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
     }
 
     private void checkAnswer(boolean userPressedTrue) {
